56f0b6e36e90a2c0afcedd87cebb38f363ad571a
DaffaPoseidon <ndap.cahyo@gmail.com>
2024-12-15 16:49:45 +0700

    Versi 1.4 (env Frontend)

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -31,7 +31,8 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:5000/auth/login`, {
+      // const response = await fetch(`http://localhost:5000/auth/login`, {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_BASEURL}/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
